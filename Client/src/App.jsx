a7355d3b7457ca30bdbae0434b8846f3e704c022
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-03-02 16:31:26 +0530

    router added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,24 @@
+import {BrowserRouter , Route , Routes} from 'react-router-dom'
+import Home from './Pages/Home';
+import AboutUs from './Pages/AboutUs';
+import ContactUs from './Pages/ContactUs';
+import Dashbord from './Pages/Dashbord';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+
 export default function App() {
   return (
     <div>
-      <h1 className="text-red-700">Hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about-us' element={<AboutUs/>}/>
+          <Route path='/contact-us' element={<ContactUs/>}/>
+          <Route path='/dashbord' element={<Dashbord/>}/>
+          <Route path='/sign-in' element={<SignIn/>}/>
+          <Route path='/sign-up' element={<SignUp/>}/>
+        </Routes>      
+      </BrowserRouter>
     </div>
   )
 }
---
+++ Client/src/Pages/Dashbord.jsx
@@ -0,0 +1,5 @@
+export default function Dashbord() {
+  return (
+    <div>Dashbord</div>
+  )
+}
