aff9a88d20d7947124d0aab61df91db5bf1304a7
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-03-04 06:41:35 +0530

    some changes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,11 +5,13 @@ import ContactUs from './Pages/ContactUs';
 import Dashbord from './Pages/Dashbord';
 import SignIn from './Pages/SignIn';
 import SignUp from './Pages/SignUp';
+import Header from './Components/Header';
 
 export default function App() {
   return (
     <div>
       <BrowserRouter>
+      <Header/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/about-us' element={<AboutUs/>}/>
