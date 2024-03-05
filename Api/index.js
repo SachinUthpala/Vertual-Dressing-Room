c522d854b7bd6e7c410637fad4e95eb88a948f33
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-03-05 17:49:43 +0530

    mongodb connected

--- Api/index.js
+++ Api/index.js
@@ -1,7 +1,18 @@
 import express from 'express'
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express()
+
+mongoose.connect(process.env.MONGO).then(
+    () => {
+        console.log("DDatabase is Connected 🌍");
+    }
+).catch((err) => {
+    console.log(err);
+})
 
 app.listen(3000,()=>{
     console.log("Server Is Running On Port 3000 ❤️");
