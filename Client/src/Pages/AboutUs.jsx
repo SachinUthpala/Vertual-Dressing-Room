26a3a6c6bc7ba9a27ddad0965766d681fa26d780
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-03-12 15:18:32 +0530

    About us page

--- Client/src/Pages/AboutUs.jsx
+++ Client/src/Pages/AboutUs.jsx
@@ -1,5 +1,47 @@
 export default function AboutUs() {
   return (
-    <div>AboutUs</div>
+    <div className="m-12">
+      <div>
+        <h1 className="text-3xl text-center font-bold mb-10">About Us</h1>
+        
+        <div className="flex gap-5 text-center ">
+          <div className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Fugit at facilis nemo natus harum. Incidunt voluptatum, tempora 
+            voluptate nesciunt tempore ratione, nihil eos error numquam cupiditate, reiciendis impedit
+             omnis accusantium! Sed doloribus in iure illum culpa nesciunt quisquam reiciendis ratione 
+             modi recusandae. Accusamus ipsam fuga dolores consequatur, quaerat impedit? Aut ipsum 
+             corrupti doloremque saepe, placeat eaque rerum sapiente fugit cum enim itaque neque inventore quasi hic voluptate qui laudantium tempore quis. Facilis nostrum, dolor cumque quasi et inventore corrupti officiis voluptatum cupiditate dolorum esse molestiae facere voluptatem minus est adipisci dolores non deserunt 
+            labore nobis sapiente. Fuga, impedit. Numquam, accusamus.
+
+            
+          </div>
+          <div className="">
+            <img src="./public/women.webp" alt=""  width={1500} />
+          </div>
+        </div>
+
+        <div className="flex gap-5 text-center ">
+          <div className="">
+            <img src="./public/women.webp" alt=""  width={1500} />
+        </div>
+          <div className="">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Fugit at facilis nemo natus harum. Incidunt voluptatum, tempora 
+            voluptate nesciunt tempore ratione, nihil eos error numquam cupiditate, reiciendis impedit
+             omnis accusantium! Sed doloribus in iure illum culpa nesciunt quisquam reiciendis ratione 
+             modi recusandae. Accusamus ipsam fuga dolores consequatur, quaerat impedit? Aut ipsum 
+             corrupti doloremque saepe, placeat eaque rerum sapiente fugit cum enim itaque neque inventore quasi hic voluptate qui laudantium tempore quis. Facilis nostrum, dolor cumque quasi et inventore corrupti officiis voluptatum cupiditate dolorum esse molestiae facere voluptatem minus est adipisci dolores non deserunt 
+            labore nobis sapiente. Fuga, impedit. Numquam, accusamus.
+          </div>
+          </div>
+      </div>
+
+      <div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
   )
 }
