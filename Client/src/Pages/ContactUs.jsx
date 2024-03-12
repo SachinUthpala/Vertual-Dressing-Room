74bc6789356819f732bd21290c6806cc47a73d3f
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-03-12 13:16:27 +0530

    create a contact us form

--- Client/src/Pages/ContactUs.jsx
+++ Client/src/Pages/ContactUs.jsx
@@ -1,5 +1,89 @@
+import { Button, Label, TextInput, Textarea } from "flowbite-react";
+import { FaMessage} from "react-icons/fa6";
+import { LuPhoneCall } from "react-icons/lu";
+import { MdOutlineMailLock } from "react-icons/md";
+import { FaAddressCard } from "react-icons/fa";
+
 export default function ContactUs() {
   return (
-    <div>ContactUs</div>
+    <div className="p-5">
+
+        <div>
+
+        </div>
+
+        <div className="text-center">
+          <h1 className="text-3xl bold text-blue-600">Contact Us</h1>
+          <p className="pb-3 border-b-8">😊😊 Feel free to contacct us any time. We are looking for help you. 😊😊 </p>
+        </div>
+
+        <div className="flex mt-4">
+
+          <div className="flex-1 border-r-2 border-blue-600 border-dashed">
+            <div className="text-center mt-2 mb-2">
+              <h1 className="text-2xl bold text-blue-600">Contact Us</h1>
+              <p>You can contact us quickly using below informations</p>
+            </div>
+            <div className="m-10 flex flex-col gap-6">
+
+              <div className="text-center">
+                <div className="flex items-center gap-4">
+                  <LuPhoneCall className="text-3xl text-blue-600"/>
+                  <span className="text-lg"> +94 762 712200 , +94 701 2345</span>
+                </div>
+                <span> You Can Contact us 24/7. Our Support Team will Help You</span>
+              </div>
+
+              <div className="text-center">
+                <div className="flex items-center gap-4">
+                  <MdOutlineMailLock className="text-3xl text-blue-600"/>
+                  <span className="text-lg"> Sachingunasekare12345@gmail.com</span>
+                </div>
+                <span> You Can Contact us 24/7. Our Support Team will Help You</span>
+              </div>
+
+              <div className="text-center">
+                <div className="flex items-center gap-4">
+                  <FaAddressCard className="text-3xl text-blue-600"/>
+                  <span className="text-lg"> No 39 , New Malkaduwawa , Kurunegala</span>
+                </div>
+                <span> You Can Contact us 24/7. Our Support Team will Help You</span>
+              </div>
+
+              
+            </div>
+          </div>
+
+          <form className="flex-col flex gap-6 mb-4 p-6 flex-1" >
+
+            <div>
+              <Label value="Your Name "/>
+              <TextInput type="text" placeholder="Your Name" id="userMail" />
+            </div>
+
+            <div>
+              <Label value="Your Email "/>
+              <TextInput type="email" placeholder="Your Email " id="userPassword" />
+            </div>
+
+            <div>
+              <Label value="Your Massage "/>
+              <Textarea type="text" placeholder="Enter your massage here ..." id="userPassword" />
+            </div>
+
+            
+
+            <Button gradientDuoTone="purpleToBlue" type="submit"  outline>
+              <div className="flex gap-3 items-center">
+                <span>Send Message</span>
+                <FaMessage/>
+              </div>
+            </Button>
+
+          </form>
+
+        </div>
+
+    </div>
   )
 }
