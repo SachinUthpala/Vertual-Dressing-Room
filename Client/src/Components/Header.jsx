a649590fa0b24c647d5d9925333717c691977389
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-03-12 11:53:22 +0530

    default

--- Client/src/Components/Header.jsx
+++ Client/src/Components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
 
   const path = useLocation().pathname;
 
-  return (
+    return (
     <div className='sticky top-0 z-10'>
       <Navbar className="border-b-2 mb-3 bg-gray-100/95">
 
@@ -34,14 +34,14 @@ export default function Header() {
           <Button className='w-12 h-11 hidden sm:inline' color='gray' pill>
             <BsMoonStars/>
           </Button>
-          <Link to={'sign-in'}>
-            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-              Sign In
-              </span>
-            </button>
-          </Link>
-          <Navbar.Toggle/>
+              <Link to={'sign-in'}>
+                  <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                      <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                      Sign In
+                      </span>
+                  </button>
+              </Link>
+              <Navbar.Toggle/>
         </div>
 
         <Navbar.Collapse>
