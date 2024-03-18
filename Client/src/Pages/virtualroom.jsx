5fe0b23d128441a789d4582c6f43b9af86afa76a
dkavish <dumidu805@gmail.com>
2024-03-18 14:40:47 +0530

    Adding virtualroom.jsx

---
+++ Client/src/Pages/virtualroom.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Example functional component using JSX
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p>This is a JSX example.</p>
+    </div>
+  );
+}
