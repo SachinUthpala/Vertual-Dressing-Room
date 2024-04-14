d561ff0b6e674b08319b16e64b85a80676cd556c
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-04-14 10:00:51 +0530

    mongo db connected

--- Api/index.js
+++ Api/index.js
@@ -1,4 +1,15 @@
 import  express  from "express";
+import { mongoose } from 'mongoose';
+
+
+mongoose.connect('mongodb+srv://gunasekarasuda:IszLHmkg1oFoWoVy@sachin.xeead9z.mongodb.net/?retryWrites=true&w=majority&appName=Sachin'
+).then(
+    () => {
+        console.log("MongoDB Connected");
+    }
+).catch((err) => {
+    console.log(err);
+})
 
 const app = express();
 app.listen(3000 , () => {
