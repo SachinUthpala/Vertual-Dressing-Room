13cc3e42a8d04b3387c327ee3888c3b3229cd0c0
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-04-14 08:33:19 +0530

    install react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import SignUp from './Pages/SignUp';
+import SignIn from './Pages/SignIn';
+
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/signin' element={<SignIn/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
