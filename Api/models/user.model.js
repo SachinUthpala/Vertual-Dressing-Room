838feda13b805481b00cb3116e1c5a5678b27aa2
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-04-14 10:55:13 +0530

    user model created

---
+++ Api/models/user.model.js
@@ -0,0 +1,21 @@
+import { mongoose } from "mongoose";
+
+const userShema = new mongoose.Shema({
+    username : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required : true,
+    }
+},{timestamps : true});
+
+const User = mongoose.model('User' , userShema);
+export default User;
