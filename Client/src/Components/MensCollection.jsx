c4587cebc45dfa8f7ebea457041839fcbc291691
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-04-14 07:28:36 +0530

    some changes

--- Client/src/Components/MensCollection.jsx
+++
@@ -1,6 +0,0 @@
-
-export default function MensCollection() {
-  return (
-    <div>MensCollection</div>
-  )
-}
--- Client/src/Pages/Dashbord.jsx
+++
@@ -1,5 +0,0 @@
-export default function Dashbord() {
-  return (
-    <div>Dashbord</div>
-  )
-}
