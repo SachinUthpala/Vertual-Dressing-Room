7055e0517740056eeca06b00bfbe5b2839c1586d
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-04-15 09:18:56 +0530

    complete signin page functionality

--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -1,7 +1,119 @@
 
+import { Link } from 'react-router-dom';
+import { Label } from 'flowbite-react';
+import { TextInput } from 'flowbite-react';
+import { Button } from 'flowbite-react';
+import { useState } from 'react';
+import { Alert } from 'flowbite-react';
+import { Spinner , } from 'flowbite-react';
+import { useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
+  //navigation
+  const navigate = useNavigate();
+  //data handling
+  const [formData , setFormData] = useState({});
+  const handleChange = (e) => {
+    setFormData({...formData , [e.target.id] : e.target.value});
+    
+  }
+  console.log(formData);
+  //submit the form
+  const handleSubmit = async (e) => {
+    e.preventDefault();//to dont refresh the page
+    if( !formData.password || !formData.email){
+      return setErrorMsg('Fill all the feilds');
+    }
+    try{
+      setErrorMsg(null);
+      setloading(true);
+      const res = await fetch('/api/auth/signin' , {
+        method : 'POST',
+        headers : {'Content-Type' : 'application/json'},
+        body : JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if(data.success === false){
+        setloading(false)
+        return setErrorMsg(data.message);
+      }
+      setloading(false);
+      if(res.ok){
+        navigate('/')
+      }
+    }catch (error){
+      console.log(error);
+      setloading(false);
+    }
+  }
+  //error handling
+  const [errorMsg , setErrorMsg] = useState(null);
+  const [loading , setloading] = useState(false);
   return (
-    <div>SignIn</div>
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 mx-auto max-w-3xl flex-col sm:flex-row items-center gap-5">
+
+        {/* left  */}
+        <div className="flex-1">
+          <Link to={'/'} className=' font-bold dark:text-white text-4xl'>
+            <span className='px-2 mr-1 bg-gradient-to-r from-cyan-500 via-blue-600 to-blue-700 text-white rounded-lg'>
+              FITTON
+            </span>
+            YOU
+          </Link>
+          <p className="text-sm mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, hic?</p>
+          <img src="public/cSignImg.png"width={200} height={200} alt="" />
+        </div>
+
+        {/* right side */}
+        <div className="flex-1">
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            <div className="">
+              <Label value='Your Email' />
+              <TextInput
+                type='email'
+                placeholder='Email'
+                id='email'
+                onChange={handleChange}
+              />
+            </div>
+            <div className="">
+              <Label value='Your Password' />
+              <TextInput
+                type='password'
+                placeholder='Password'
+                id='password'
+                onChange={handleChange}
+              />
+            </div>
+            <Button gradientDuoTone='purpleToBlue' outline type='submit' disabled={loading}>
+              {
+                loading ? (
+                  <>
+                  <Spinner size='sm' />
+                  <span className='pl-2'>Loading</span>
+                  </>
+                ) : 'Sign In'
+              }
+            </Button>
+          </form>
+          <div className="flex gap-2 mt-3 text-sm font-semibold">
+            <span>Dont tHave An Account : </span>
+            <Link to={'../signup'} className='text-blue-500' >
+              Sign Up
+            </Link>
+          </div>
+          {
+            errorMsg && (
+              <Alert className='mt-5' color='failure'>
+                {errorMsg}
+              </Alert>
+            )
+          }
+        </div>
+
+      </div>
+    </div>
   )
 }
