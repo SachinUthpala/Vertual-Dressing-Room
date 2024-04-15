5fe8cc31dd9ee719715088f335430e856b575584
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-04-15 09:08:59 +0530

    create sign in api route

--- Api/controler/auth.controler.js
+++ Api/controler/auth.controler.js
@@ -1,7 +1,9 @@
 
 import {errorHandler}  from './../utils/error.js';
 import User from './../models/user.model.js';
-import bryptjs from 'bcryptjs'
+import bcryptjs from 'bcryptjs'; 
+import jwt from 'jsonwebtoken'
+
 
 export const signUp = async (req , res , next) => {
     const {
@@ -38,3 +40,72 @@ export const signUp = async (req , res , next) => {
     }
 }
 
+
+// export const signIn = async (req , res , next) => {
+//     const {email , password} = req.body;
+
+//     if(!email || !password || email === ' ' || password === ' '){
+//         next(errorHandler(400 , 'All Feilds are required !'));
+//     }
+
+//     try {
+//         const validUser = await User.findOne({email});
+        
+//         if(!validUser){
+//             next(errorHandler(404 , 'User Not Found'));
+//         }
+
+//         const validPassword = bryptjs.compareSync(password , validUser.password);
+
+//         if(!validPassword){
+//             return next(errorHandler(400 , 'Invallied Password'));
+//         }
+
+//         const token = jwt.sign({
+//             id : validUser._id
+//         } , process.env.JWT_SECRETKEY );
+
+//         res.status(200).cookie('access_token', token, {
+//         httpOnly: true,
+//         }).json(validUser)
+
+//     } catch (error) {
+//         next(error)
+//     }
+
+
+// } 
+
+export const signIn = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password || email.trim() === '' || password.trim() === '') {
+        return next(errorHandler(400, 'All fields are required!')); // Use return to exit the function
+    }
+
+    try {
+        const validUser = await User.findOne({ email });
+
+        if (!validUser) {
+            return next(errorHandler(404, 'User Not Found')); // Use return to exit the function
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password); // Corrected spelling of bcryptjs
+
+        if (!validPassword) {
+            return next(errorHandler(400, 'Invalid Password')); // Corrected spelling of Invalid
+        }
+
+        const token = jwt.sign({
+            id: validUser._id
+        }, "Sachin");
+
+        const { password : pass , ...rest } = validUser._doc;
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true,
+        }).json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
--- Api/routs/auth.rout.js
+++ Api/routs/auth.rout.js
@@ -1,8 +1,9 @@
 import  express  from 'express';
-import {signUp} from './../controler/auth.controler.js';
+import {signIn, signUp} from './../controler/auth.controler.js';
 
 const router = express.Router();
 
-router.post('/signup' , signUp)
+router.post('/signup' , signUp);
+router.post('/signin' , signIn );
 
 export default router;
