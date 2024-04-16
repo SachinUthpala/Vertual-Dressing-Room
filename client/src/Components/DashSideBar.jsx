a435bd77bdbc6f6870f6d33db726eaf552cd6f62
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-04-16 19:41:34 +0530

    complete side bar of the dashbord

---
+++ client/src/Components/DashSideBar.jsx
@@ -0,0 +1,45 @@
+import { Sidebar, Label } from 'flowbite-react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import {
+  HiUser,
+  HiArrowSmLeft,
+  HiDocumentText,
+  HiOutlineUserGroup,
+  HiAnnotation,
+  HiChartPie,
+} from 'react-icons/hi';
+import { Link, useLocation } from 'react-router-dom';
+
+export default function DashSideBar() {
+
+  const location = useLocation();
+  const [tab , setTab] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    console.log(tabFromUrl);
+
+    if(tabFromUrl){
+      setTab(tabFromUrl)
+    }
+  } , [location.search])
+
+  return (
+    <Sidebar className='w-full md:w-56'>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to='/dashbord?tab=profile'>
+            <Sidebar.Item active={tab === 'profile'} icon={HiUser} label='User Profile' labelColor='dark'>
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item  icon={HiArrowSmLeft} label='Sign Out' labelColor='red' className = "cursor-pointer">
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  )
+}
--- client/src/Pages/Dashbord.jsx
+++ client/src/Pages/Dashbord.jsx
@@ -1,6 +1,35 @@
 
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashSideBar from '../Components/DashSideBar';
+import DashProfile from '../Components/DashProfile';
+
+
 export default function Dashbord() {
+  const location = useLocation();
+  const [tab , setTab] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    console.log(tabFromUrl);
+
+    if(tabFromUrl){
+      setTab(tabFromUrl)
+    }
+  } , [location.search])
   return (
-    <div>Dashbord</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      {/* side bar */}
+      <div className="md:w-56">
+        <DashSideBar/>
+      </div>
+      {/* dashbord content */}
+      <div className="">
+        {
+          tab === 'profile' && <DashProfile/>
+        }
+      </div>
+    </div>
   )
 }
