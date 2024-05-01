6d0388eade23acc64a40f6a0a55a2c7413e84c95
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-05-01 16:18:21 +0530

    update user api route

--- Api/controler/user.controler.js
+++ Api/controler/user.controler.js
@@ -1,8 +1,50 @@
+import User from '../models/user.model.js';
+import { errorHandler } from './../utils/error.js';
+
 export const test = (req , res) => {
+
     res.json({message : "Router is working"});
 }
 
 export const updateUser = async (req , res , next) => {
 
     console.log(req.user);
+
+
+    if (req.user.id !== req.params.userId) {
+        return next(errorHandler(403, 'You are not allowed to update this user'));
+    }
+    if (req.body.password) {
+        if (req.body.password.length < 6) {
+          return next(errorHandler(400, 'Password must be at least 6 characters'));
+        }
+        req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    if (req.body.username) {
+        if (req.body.username.length < 7 || req.body.username.length > 20) {
+          return next(
+            errorHandler(400, 'Username must be between 7 and 20 characters')
+          );
+        }
+    }
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+          req.params.userId,
+          {
+            $set: {
+              username: req.body.username,
+              email: req.body.email,
+              profilePicture: req.body.profilePicture,
+              password: req.body.password,
+            },
+          },
+          { new: true }
+        );
+        const { password, ...rest } = updatedUser._doc;
+        res.status(200).json(rest);
+      } catch (error) {
+        next(error);
+      }
 }
